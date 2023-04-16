5fc35f9c171d003e12b9edc5c078ddf3b5e1ac2a
susinomatias <susino_matias@outlook.com>
2023-04-16 20:37:49 -0300

    cambios con prompt, confirm y alert en JS

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-alert("este sitio utiliza cookies");
-prompt("hola");
-confirm("si");             
+let alerta = alert("Bienvenido a Soyma. Recorre nuestro sitio para encontrar las pistas ocultas y ganar increibles premios");
+
+
+let confirmar = confirm("Este sitio utliza cookies para una mejor experiencia. ¿Deseas aceptarlas?");
+
+
+console.log(confirmar);
+
+if (confirmar) {
+    alert("!Gracias!");
+}
+
+let nombre = prompt("Ingrese su nombre"),
+    apellido = prompt("Ingrese su apodo"),
+    resultado;
+
+resultado = nombre + " " + apellido,
+
+    console.log(alert(resultado));
+
+
+
+
+//alert("este sitio utiliza cookies");//
+//  prompt("hola"); //
+//confirm("si");//             
